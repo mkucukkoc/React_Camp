4d28e15eedb0873b201a2151e0b4f69cc4ec92bd
Mustafa Kucukkoc <98315805+mkucukkoc@users.noreply.github.com>
2022-04-20 10:23:12 +0300

    react kampı

---
+++ src/layouts/Dashboard.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import  ProductList from '../pages/ProductList';
+import Categories from './Categories';
+import Navi from './Navi';
+export default function Dashboard() {
+  return (
+    <div>
+      <Navi/>
+      <Categories/>
+      <ProductList/>
+    </div>
+  );
+}
